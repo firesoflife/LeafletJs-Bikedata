99f8f30ac877ad85c263d833806c3f5db2fec5eb
firesoflife <bryangoertz@gmail.com>
2021-06-25 09:56:52 -0400

    styles added

--- app/javascript/components/RacerProfiles/Profile.jsx
+++ app/javascript/components/RacerProfiles/Profile.jsx
@@ -8,14 +8,25 @@ const Profile = props => {
 				{props.attributes.f_name} {props.attributes.l_name}{' '}
 			</div>
 			<div className='card'>
-				<div className='name-logo-box'>
-					<div className='profile-img'>
-						<ion-icon name='bicycle'></ion-icon>
+				<div className='profile'>
+					<ion-icon name='bicycle'></ion-icon>
+					<div className='info-box'>
+						<h3 className='info-title'>Racer Info</h3>
+						<div className='line-break'></div>
+						<div className='racer-city'>
+							<span className='lead'>Home Town: </span>
+							<br></br>
+							{props.attributes.city}{' '}
+						</div>
+						<div className='racer-state'>
+							<span className='lead'>Home State: </span>
+							{props.attributes.state}{' '}
+						</div>
 					</div>
 				</div>
-				<div className='racer-city'>{props.attributes.city} </div>
-				<div className='racer-state'>{props.attributes.state} </div>
-				<div className='racer-location'>Check Map!</div>
+				<div className='racer-location'>
+					Find {props.attributes.f_name} on the Map!
+				</div>
 			</div>
 		</div>
 	);
