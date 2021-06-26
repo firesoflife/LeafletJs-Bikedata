0ab292108a9697b8a319ea0654a85b2f0fb87394
firesoflife <bryangoertz@gmail.com>
2021-06-25 21:50:23 -0400

    racer locations with markers and popup sample added

--- app/javascript/components/RaceMap.jsx
+++ app/javascript/components/RaceMap.jsx
@@ -1,7 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import axios from 'axios';
 
-const RaceMap = () => {
+const RaceMap = props => {
+	const [position, setPosition] = useState([]);
+
+	useEffect(() => {
+		axios
+			.get('/api/v1/racers.json')
+			.then(resp => {
+				setPosition(resp.data.data);
+				// console.log(resp.data.data);
+			})
+			.catch(resp => console.log(resp));
+	}, [position.length]);
+
+	const marker = position.map(racerPosition => {
+		return (
+			<Fragment>
+				<Marker
+					key={racerPosition.id}
+					position={[
+						racerPosition.attributes.lat,
+						racerPosition.attributes.lng,
+					]}
+				>
+					<Popup>{racerPosition.attributes.f_name}</Popup>
+				</Marker>
+			</Fragment>
+		);
+	});
+
 	return (
 		<div>
 			<h1 className='title'>Track Your Favourite Riders</h1>
@@ -14,11 +43,13 @@ const RaceMap = () => {
 					attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 					url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
 				/>
-				<Marker position={[42.983, -81.249]}>
+
+				{/* <Marker position={[42.983, -81.249]}>
 					<Popup>
 						A pretty CSS3 popup. <br /> Easily customizable.
 					</Popup>
-				</Marker>
+				</Marker> */}
+				{marker}
 			</MapContainer>
 		</div>
 	);
